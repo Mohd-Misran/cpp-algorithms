33519b2665fbf52c7fa192a1e3a719ce0780459a
Mohammed Misran <mohammed.misran38@gmail.com>
2021-03-22 11:53:48 +0530

    Sieve of Eratosthenes Algorithm

---
+++ sieveOfEratosthenes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void sieveOfEratosthenes(int n) {
+
+    bool *prime = new bool[n + 1];
+    for (int i = 0; i < n + 1; i++)
+        prime[i] = true;
+  
+    for (int p = 2; p *p <= n; p++) {
+    
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+  
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            cout << p << " ";
+}
+
+int main() 
+{
+    int n;
+    cout << "Enter value of n: ";
+  	cin >> n;
+  	sieveOfEratosthenes(n);
+    return 0;
+}
