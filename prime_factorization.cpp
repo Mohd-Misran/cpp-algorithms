bdd66cd32863cba3ffc4121bb4d04adff45e96c2
Mohammed Misran <mohammed.misran38@gmail.com>
2021-03-23 12:46:42 +0530

    All prime factors of a number

---
+++ prime_factorization.cpp
@@ -0,0 +1,35 @@
+// All prime factors of a number
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void primeFactors(int n) {
+
+    while (n % 2 == 0) {
+        cout << 2 << " ";
+        n = n / 2;
+    }
+
+    for (int i = 3; i <= sqrt(n); i = i + 2) {
+
+        while (n % i == 0) {
+            cout << i << " ";
+            n = n / i;
+        }
+    }
+
+    if (n > 2)
+        cout << n;
+}
+
+int main() {
+
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+    cout << "Prime factors of " << n << " are " << endl;
+    primeFactors(n);
+    return 0;
+}
